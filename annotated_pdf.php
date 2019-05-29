244402012d4637c4363d1c4bfbee092818aef125
Luke <lukestevens@hotmail.com>
2019-05-29 11:01:45 +1000

    Update annotated_pdf.php
    Now preferring "PDF" to "CRF" as more general

--- annotated_pdf.php
+++ annotated_pdf.php
@@ -1,3 +1,3 @@
 <?php
-if (is_null($module) || !($module instanceof MCRI\AnnotatedCRF\AnnotatedCRF)) { exit(); }
+if (is_null($module) || !($module instanceof MCRI\AnnotatedPDF\AnnotatedPDF)) { exit(); }
 $module->renderAnnotatedPdf();
