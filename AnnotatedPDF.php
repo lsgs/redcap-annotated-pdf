88222aeabc3f79b62def8812d33fed4c5bd8c287
Luke <lukestevens@hotmail.com>
2020-04-09 11:12:13 +1000

    Use redcap_pdf hook
    Simplify module to use the redcap_pdf hook available from REDCap v9.5.0.

--- AnnotatedPDF.php
+++ AnnotatedPDF.php
@@ -12,9 +12,12 @@ class AnnotatedPDF extends AbstractExternalModule
 {
         public function redcap_every_page_top($project_id) {
                 if (PAGE==='Design/data_dictionary_codebook.php') {
-                        $url = $this->getUrl('annotated_pdf.php');
+                        $url = (version_compare(REDCAP_VERSION, '9.8.0')<0) 
+                                ? APP_PATH_WEBROOT_FULL.'redcap_v'.REDCAP_VERSION.'/PDF/index.php?annotated=1&pid='.$project_id
+                                : APP_PATH_WEBROOT_FULL.'redcap_v'.REDCAP_VERSION.'/index.php?route=PdfController/index&annotated=1&pid='.$project_id;
+
                         ?>
-                        <button id="annotated-crf-btn" class="btn btn-defaultrc btn-xs">
+                        <button id="annotated-crf-btn" class="btn btn-defaultrc btn-xs invisible_in_print">
                             <img src="<?php echo APP_PATH_IMAGES;?>/pdf.gif" style="vertical-align:middle;"/> 
                             <span style="font-size:12px;vertical-align:middle;">Annotated PDF</span>
                         </button>
@@ -35,107 +38,12 @@ class AnnotatedPDF extends AbstractExternalModule
                 }
         }
         
-        public function renderAnnotatedPdf() {
-                global $app_title;
-
-                // replicating font stuff from PDF/index.php
-                // Must have PHP extention "mbstring" installed in order to render UTF-8 characters properly AND also the PDF unicode fonts installed
-                $pathToPdfUtf8Fonts = APP_PATH_WEBTOOLS . "pdf" . DS . "font" . DS . "unifont" . DS;
-                if (function_exists('mb_convert_encoding') && is_dir($pathToPdfUtf8Fonts)) {
-                        // Define the UTF-8 PDF fonts' path
-                        define("FPDF_FONTPATH",   APP_PATH_WEBTOOLS . "pdf" . DS . "font" . DS);
-                        define("_SYSTEM_TTFONTS", APP_PATH_WEBTOOLS . "pdf" . DS . "font" . DS);
-                        // Set contant
-                        define("USE_UTF8", true);
-                        // Use tFPDF class for UTF-8 by default
-                        if ($project_encoding == 'chinese_utf8') {
-                                require_once APP_PATH_LIBRARIES . "PDF_Unicode.php";
-                        } else {
-                                require_once APP_PATH_LIBRARIES . "tFPDF.php";
-                        }
-                } else {
-                        // Set contant
-                        define("USE_UTF8", false);
-                        // Use normal FPDF class
-                        require_once APP_PATH_LIBRARIES . "FPDF.php";
-                }
-                // If using language 'Japanese', then use MBFPDF class for multi-byte string rendering
-                if ($project_encoding == 'japanese_sjis')
-                {
-                        require_once APP_PATH_LIBRARIES . "MBFPDF.php"; // Japanese
-                        // Make sure mbstring is installed
-                        if (!function_exists('mb_convert_encoding'))
-                        {
-                                exit("ERROR: In order for multi-byte encoded text to render correctly in the PDF, you must have the PHP extention \"mbstring\" installed on your web server.");
-                        }
-                }
+        public function redcap_pdf($project_id, $metadata, $data, $instrument, $record, $event_id, $instance) {
+                if (!(isset($_GET['annotated']) && (bool)$_GET['annotated'])) return;
                 
-                require_once APP_PATH_DOCROOT . "PDF/functions.php"; // This MUST be included AFTER we include the FPDF class
-                
-                // Remove special characters from title for using as filename
-                $filename .= str_replace(" ", "", ucwords(preg_replace("/[^a-zA-Z0-9]/", " ", $app_title)));
-                // Make sure filename is not too long
-                if (strlen($filename) > 30) {
-                        $filename = substr($filename, 0, 30);
-                }
-                
-                $acknowledgement = getAcknowledgement($_GET['pid'], (isset($_GET['instrument']))?$_GET['instrument']:'');
-
-                header('Content-Type: application/x-download');
-                header('Content-Disposition: attachment; filename="'.$filename.'Annotated.pdf"');
-                header('Cache-Control: private, max-age=0, must-revalidate');
-                header('Pragma: public');
-
-                $metadata = $this->getMetadataForPdf();
-                
-                renderPDF($metadata, $acknowledgement, strip_tags(label_decode($app_title).'_annotated'), array(''=>array(''=>null)), false);
-        }
-        
-        protected function getMetadataForPdf() {
-                global $project_id, $Proj, $table_pk;
-                
-                // replicating metadata read from PDF/index.php
-                // Save fields into metadata array
-                $draftMode = false;
-                if (isset($_GET['instrument'])) {
-                        // Check if we should get metadata for draft mode or not
-                        $draftMode = ($status > 0 && isset($_GET['draftmode']));
-                        $metadata_table = ($draftMode) ? "redcap_metadata_temp" : "redcap_metadata";
-                        // Make sure form exists first
-                        if ((!$draftMode && !isset($Proj->forms[$_GET['instrument']])) || ($draftMode && !isset($Proj->forms_temp[$_GET['instrument']]))) {
-                                exit('ERROR!');
-                        }
-                        $Query = "select * from $metadata_table where project_id = $project_id and ((form_name = '{$_GET['instrument']}'
-                                          and field_name != concat(form_name,'_complete')) or field_name = '$table_pk') order by field_order";
-                } else {
-                        $Query = "select * from redcap_metadata where project_id = $project_id and
-                                          (field_name != concat(form_name,'_complete') or field_name = '$table_pk') order by field_order";
-                }
-                $QQuery = db_query($Query);
-                $metadata = array();
-                while ($row = db_fetch_assoc($QQuery))
-                {
-                        // If field is an "sql" field type, then retrieve enum from query result
-                        if ($row['element_type'] == "sql") {
-                                $row['element_enum'] = getSqlFieldEnum($row['element_enum'], PROJECT_ID, $_GET['id'], $_GET['event_id'], $_GET['instance'], null, null, $_GET['instrument']);
-                        }
-                        // If PK field...
-                        if ($row['field_name'] == $table_pk) {
-                                // Ensure PK field is a text field
-                                $row['element_type'] = 'text';
-                                // When pulling a single form other than the first form, change PK form_name to prevent it being on its own page
-                                if (isset($_GET['instrument'])) {
-                                        $row['form_name'] = $_GET['instrument'];
-                                }
-                        }
-                        // Store metadata in array
-                        $metadata[] = $row;
-                }
-                
-                
-                // now tweaking the element label and value labels for annotation purposes
-                $annotated = array();
-                foreach ($metadata as $fld => $fieldattr) {
+                // tweak metadata element label and value labels for annotation purposes
+                $annotatedMetadata = array();
+                foreach ($metadata as $fieldIndex => $fieldattr) {
                         $annotation = '';
                         
                         if ($fieldattr['element_type']!=='descriptive') {
@@ -163,9 +71,9 @@ class AnnotatedPDF extends AbstractExternalModule
                                 $fieldattr['element_enum'] = implode('\n', $choicesannotated);
                         }
                         
-                        $annotated[$fld] = $fieldattr;
+                        $annotatedMetadata[$fieldIndex] = $fieldattr;
                 }
-                
-                return $annotated;
+
+                return array('metadata' => $annotatedMetadata);
         }
 }
